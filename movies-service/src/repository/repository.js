547cae5a9083cdd8cc78de293012b67a1b6c1031
kenpeter <figo2476@hotmail.com>
2017-06-22 09:02:06 +1000

    [feature] comments

--- movies-service/src/repository/repository.js
+++ movies-service/src/repository/repository.js
@@ -28,36 +28,56 @@ const repository = (db) => {
       const sendMovies = (err) => {
         // error
         if (err) {
-          // reject 
+          // reject
           reject(new Error('An error occured fetching all movies, err:' + err))
         }
+
+        // resolve, slice that movie
         resolve(movies.slice())
       }
+
+      // cursor after find
+      // each add and remove, 2????????????
       cursor.forEach(addMovie, sendMovies)
     })
   }
 
+  // const again
+  // get movie pre
   const getMoviePremiers = () => {
+    // promise
     return new Promise((resolve, reject) => {
+      // mov array
       const movies = []
+      // today date
       const currentDay = new Date()
+      // release year
+      // 2016 < x <= 2017
       const query = {
         releaseYear: {
           $gt: currentDay.getFullYear() - 1,
           $lte: currentDay.getFullYear()
         },
+        // month
+        // 6 + 1 < x <= 6 + 2
         releaseMonth: {
           $gte: currentDay.getMonth() + 1,
           $lte: currentDay.getMonth() + 2
         },
+        // date
+        // <= 22
         releaseDay: {
           $lte: currentDay.getDate()
         }
       }
+
+      // another cursor with query
       const cursor = collection.find(query)
+      // add movies same
       const addMovie = (movie) => {
         movies.push(movie)
       }
+      // send movie same
       const sendMovies = (err) => {
         if (err) {
           reject(new Error('An error occured fetching all movies, err:' + err))
@@ -68,23 +88,32 @@ const repository = (db) => {
     })
   }
 
+  // get movie by i
   const getMovieById = (id) => {
+    // pro
     return new Promise((resolve, reject) => {
+      // what _id, id, title, format
       const projection = { _id: 0, id: 1, title: 1, format: 1 }
+      // promise
       const sendMovie = (err, movie) => {
         if (err) {
           reject(new Error(`An error occured fetching a movie with id: ${id}, err: ${err}`))
         }
         resolve(movie)
       }
+      // collection find one
+      // with id as obj, proj?????
+      // send movie, resolve movie
       collection.findOne({id: id}, projection, sendMovie)
     })
   }
 
+  // dis db
   const disconnect = () => {
     db.close()
   }
 
+  // all objcreate
   return Object.create({
     getAllMovies,
     getMoviePremiers,
@@ -93,13 +122,18 @@ const repository = (db) => {
   })
 }
 
+// now promise connect with connection
 const connect = (connection) => {
+  // pro
   return new Promise((resolve, reject) => {
+    // error
     if (!connection) {
       reject(new Error('connection db not supplied!'))
     }
+    // see fun above
     resolve(repository(connection))
   })
 }
 
+// new obj, {}, {connect}
 module.exports = Object.assign({}, {connect})
---
+++ test/my_slice.js
@@ -0,0 +1,6 @@
+var fruits = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'];
+var citrus = fruits.slice(1);
+var citrus1 = fruits.slice(1, 3);
+
+console.log(citrus);
+console.log(citrus1);
